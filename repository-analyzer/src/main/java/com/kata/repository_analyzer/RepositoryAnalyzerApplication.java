aad69c08592b6f73309bb3df10a2724976adec8d
SantiagoOrtega <43933691+SantiagoOrtega@users.noreply.github.com>
2026-09-03 15:18:48 -0500

    initial commit

---
+++ repository-analyzer/src/main/java/com/kata/repository_analyzer/RepositoryAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.kata.repository_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryAnalyzerApplication.class, args);
+	}
+
+}
---
+++ repository-analyzer/src/test/java/com/kata/repository_analyzer/RepositoryAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.repository_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
